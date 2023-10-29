98a1c855b4f7d0d730dc705c70d721f2fb0fae05
Matthew MacLeod <mgmacleod8@gmail.com>
2023-10-29 19:05:55 -0400

    remove generated file

--- print_that_shit.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Load the csv file
-df = pd.read_csv("log_wavy.csv")
-
-# Print the 'prompt' and 'filename' columns
-for index, row in df.iterrows():
-    print(f"Prompt: {row['prompt']}, Filename: {row['filename']}")
