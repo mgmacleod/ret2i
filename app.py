5a755494b9fe6e73800a7ecc1b7c69541ab39be4
Matthew MacLeod <mgmacleod8@gmail.com>
2023-10-28 23:51:03 -0400

    better prompts and ignoring outputs

--- app.py
+++ app.py
@@ -26,14 +26,14 @@ user_proxy = autogen.UserProxyAgent(
     human_input_mode="TERMINATE",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": "out"},
+    code_execution_config={"work_dir": "out", "use_docker": False},
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
 Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
 )
 
 task = """
-Write python code to output numbers 1 to 100, and then store the code in a file
+Write python code to output numbers 1 to 100, confirm that the code works, and then store the code in a .py file.
 """
 
 user_proxy.initiate_chat(assistant, message=task)
