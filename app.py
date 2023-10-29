a52c95fa56e4bdcecc3b8b8f27da133c8f881282
Matthew MacLeod <mgmacleod8@gmail.com>
2023-10-28 23:32:11 -0400

    using tgwui

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import autogen
 
-config_list = [{"model": "gpt-4", "api_key": "API_KEY"}]
+config_list = [
+    {
+        "api_type": "open_ai",
+        "api_base": "http://192.168.0.110:5001/v1",
+        "api_key": "sk-111111111111111111111111111111",
+    }
+]
 
 llm_config = {
     "request_timeout": 600,
@@ -10,17 +16,17 @@ llm_config = {
 }
 
 assistant = autogen.AssistantAgent(
-    name="CTO",
+    name="Coder",
     llm_config=llm_config,
-    system_message="Chief technical officer of a tech company",
+    system_message="You are a coder specializing in Python",
 )
 
 user_proxy = autogen.UserProxyAgent(
     name="user_proxy",
-    human_input_mode="NEVER",
+    human_input_mode="TERMINATE",
     max_consecutive_auto_reply=10,
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
-    code_execution_config={"work_dir": "web"},
+    code_execution_config={"work_dir": "out"},
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
 Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
@@ -32,8 +38,8 @@ Write python code to output numbers 1 to 100, and then store the code in a file
 
 user_proxy.initiate_chat(assistant, message=task)
 
-task2 = """
-Change the code in the file you just created to instead output numbers 1 to 200
-"""
+# task2 = """
+# Change the code in the file you just created to instead output numbers 1 to 200
+# """
 
-user_proxy.initiate_chat(assistant, message=task2)
+# user_proxy.initiate_chat(assistant, message=task2)
